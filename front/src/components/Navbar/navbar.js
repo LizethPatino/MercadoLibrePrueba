b32c164cbf72dfff3dbdb9aeab416c878246a9dc
Your Name <lizeth0330@outlook.com>
2021-01-08 19:09:16 -0500

    Responsabilidad unica para la barra de busqueda

--- front/src/components/Navbar/navbar.js
+++ front/src/components/Navbar/navbar.js
@@ -1,42 +1,35 @@
  
-import React, {useState, useEffect, useCallback } from "react";
+import React, {useState} from "react";
 import logoMeli from '../../Images/Logo_ML.png';
+import {URL_API} from '../../Constants/UrlApi';
 import lupita from '../../Images/ic_Search.png';
 import './navbar.scss'
 
 
-/*Estado inicial*/
-const Search = () => {
-  const [appState, setAppState] = useState({
-    loading: false,
-    repos: null,
-  });
-  
- /* LLamado a la API */ 
-const callApi = useCallback(
-  () => {
-    setAppState({ loading: true });
-    const apiUrl = `http://localhost:3001/api/items?q=Apple ipod`;
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((repos) => {
-        setAppState({ loading: false, repos: repos });
-      });
-  },
-  [],
-)
-  useEffect(() => {
-    callApi()
-  }, [callApi]);
 
+const Search = () => {
+
+  /*Estado inicial*/
+  const [item, setData] = useState('');
+
+  
+
+ /* enviar item de busqueda */
+  const sentData = (event) => {
+    event.preventDefault()
+    console.log('enviando datos...' + item)
+}
+
+  
   return (
     /*Genera la barra de busqueda*/
     <div>
         <nav className="navbar navSearch">
                 <div className="container-fluid d-flex justify-content-lg-center">
                     <img src={logoMeli} className="navbar-brand" alt='mercadolibreLogo'/>
-                    <form className="d-flex col-md-9">
-                        <input className="form-control inputSearch rounded-0" type="text" placeholder="Nunca dejes de buscar" aria-label="Search" onClick={callApi} />
+                    <form className="d-flex col-md-9" onSubmit={sentData}>
+                        <input className="form-control inputSearch rounded-0" type="text" placeholder="Nunca dejes de buscar"
+                        aria-label="Search" onChange={e => setData(e.target.value)} value={item} />
                         <button className="butonSearch" type="submit">
                           <img src={lupita} className="img-fluid" alt='buscar'/>
                         </button>
@@ -46,8 +39,6 @@ const callApi = useCallback(
     </div>
   );
 };
-
-
 
 
 export default Search;
