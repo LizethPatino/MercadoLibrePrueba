5b1391c65f96e57b70d1cfc3c554339e938f9135
Your Name <lizeth0330@outlook.com>
2021-01-08 21:12:23 -0500

    Se elimina un componente innecesario

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Template from './Components/Template/template';
+import Search from './Components/Navbar/navbar';
+import { Route ,BrowserRouter, Switch } from "react-router-dom";
+import ListItems from './Components/ResultListItems/resultListItems';
 
 function App() {
   return (
-    <div className="App">
-        <Template></Template>
-    </div>
+    <BrowserRouter>
+      <div className="background-template">
+        <Search></Search> 
+        <Switch>
+          <Route path="/t" exact component={ListItems} />
+         
+        </Switch>
+      </div>
+    </BrowserRouter>
+       
+
   );
 }
 
---
+++ front/src/components/ResultListItems/resultListItems.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const ListItems = () => {
+
+return(
+    <div>
+        <h2>HOLI!!!!</h2>
+    </div>
+);
+
+};
+
+export default ListItems;
